90e775f94877bcd1e1b0121003be5bd4955c9a10
aidenn19 <aidenn19@nycstudents.net>
2025-10-01 13:16:11 +0000

    .

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,35 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+
+  }
+
+  String print(String message) {
+	System.out.printLn(message);
+	return message:
+	
+	double Ftoc (double f ) {
+		double c = (f-32) * 9 / 5.0;
+		return c;
+}
+double sphereVolume(double r){
+	double v = (4/3.0) * MathPI * Math.pow(double r, double 3);
+	return v;
+
+double coneVolume(double r){
+	double v = (1/3.0) * Math.PI * Math.pow(r, 2) * h;
+	return v;
+
+double distance(double x1, double y1, double 2x, double y2){
+	double d = Math.sprt(Math.pow(x2 - x1, 2)+ Math.pow(y2 - y1, 2))
+	return d;
+
+double cylinderVolume = (double r, double h){
+	double v = Math.PI * Math.pow(r, 2) * h;
+	return v;
+
+}
